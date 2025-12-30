18cce8e91056f9065cefe1ee04c6117bc1bcbc30
nawfal8815 <nawfalelaghzaoui2001@gmail.com>
2025-12-30 21:16:29 +0200

    Added index.html to the / get request

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const config = require('../../config/config');
 
 const app = express();
@@ -11,8 +12,11 @@ let candles = [];
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+// 👉 serve frontend
+app.use(express.static(path.join(__dirname, "dist")));
+
 app.get("/", (req, res) => {
-  res.send("Backend is running ✅");
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.post("/api/data", (req, res) => {
